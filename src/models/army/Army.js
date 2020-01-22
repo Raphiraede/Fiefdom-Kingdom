bd75a32dd0a5a7477ed1a58e050331da7d73fdac
Raphael Raede <raphiraede@gmail.com>
2020-01-21 20:39:11 -0800

    extracted handleNextTurn functionality from methods of the army class to seperate functon in handleNextTurn.js

--- src/models/army/Army.js
+++ src/models/army/Army.js
@@ -11,14 +11,7 @@ class Army{
   }
 
 
-  handleNextTurn({state}){
-    const kingdomId = this.calculateKingdomIdThatArmyIsLoyalTo(state)
-    if(this.mode === 'move') this.moveTowardDestination(state.armies)
-    else if(this.mode === 'conquer') this.handleConquerMode(state, kingdomId)
-    if(this.turnsOnSameTile >= 2) this.conquerTerritory(state, kingdomId)
-    this.payWages(state, kingdomId)
-    this.turnsOnSameTile++
-  }
+
 
   payWages(state, kingdomId){
     const wageOwed = this.calculateTotalSize() * 10
@@ -33,42 +26,6 @@ class Army{
       totalPop += this.demographics[key]
     }
     return totalPop
-  }
-
-  moveTowardDestination(armies){
-    let targetSquare = {...this.coordinates}
-    if(this.coordinates.x < this.destination.x) targetSquare.x = this.coordinates.x + 1
-    else if (this.coordinates.x > this.destination.x) targetSquare.x = this.coordinates.x - 1
-
-    if(this.coordinates.y < this.destination.y) targetSquare.y = this.coordinates.y + 1
-    else if (this.coordinates.y > this.destination.y) targetSquare.y = this.coordinates.y - 1
-
-    const pathIsClear = this.checkIfPathIsClear(targetSquare, armies)
-    if(pathIsClear){
-      this.coordinates = targetSquare
-      this.turnsOnSameTile = 0
-    }
-  }
-
-  handleConquerMode(state, kingdomId){
-    const kingdomOwnerOfTile = state.gameMap[this.coordinates.x][this.coordinates.y].kingdomOwner
-    if(kingdomId === kingdomOwnerOfTile) this.moveTowardDestination(state.armies)
-  }
-
-  //currently the only thing which can block a path is another army
-  checkIfPathIsClear(targetSquare, armies){
-    let pathIsClear = true
-    for (const id in armies){
-      const army = armies[id]
-      if(targetSquare.x === army.coordinates.x && targetSquare.y === army.coordinates.y){
-        pathIsClear = false
-      }
-    }
-    return pathIsClear
-  }
-
-  conquerTerritory(state, kingdomId){
-    state.gameMap[this.coordinates.x][this.coordinates.y].kingdomOwner = kingdomId
   }
 
   calculateKingdomIdThatArmyIsLoyalTo(state){
--- src/redux/reducer/reducerHelpers/nextTurn/handleNextTurn.js
+++ src/redux/reducer/reducerHelpers/nextTurn/handleNextTurn.js
@@ -22,13 +22,7 @@ function handleNextTurn(state){
     noble.handleNextTurn(state.randomNameGenerator)
   })
 
-  const armies = newState.armies
-  const armyIds = Object.keys(armies)
-  }
-  armyIds.forEach(id => {
-    const army = armies[id]
-    army.handleNextTurn({state, armies})
-  })
+  handleArmiesNextTurn(state)
   newState.indexes = createIndexes(newState)
 
   aiKingdomTurn(newState)
@@ -49,6 +43,79 @@ function calculateTaxesAndUpdateGoldAmount(mainKingdom, nobles, gameMap){
       }
     }
   }
+}
+
+function handleArmiesNextTurn(state){
+
+  let armyArray = []
+  let battlesMapping = {} //maps one armyID to antoher armyID to figure out who needs to battle who.
+  for(const armyId in state.armies){
+    armyArray.push(state.armies[armyId])
+  }
+
+  armyArray.forEach(army => {
+    const targetSquare = calculateTargetSquare(army)
+    const blockingArmyId = checkIfArmyIsBlocking(army.coordinates, armyArray, army)
+    const kingdomIdWhichArmyIsLoyalTo = army.calculateKingdomIdThatArmyIsLoyalTo(state)
+
+    if(blockingArmyId){
+      const blockingArmy = state.armies[blockingArmyId]
+      const blockingArmyKingdomLoyalty = blockingArmy.calculateKingdomIdThatArmyIsLoyalTo(state)
+      if(blockingArmyKingdomLoyalty !== kingdomIdWhichArmyIsLoyalTo){
+        battlesMapping[army.id] = blockingArmyId
+      }
+    }
+    else{
+      if(army.mode === 'move'){
+        this.moveTowardDestination(army, targetSquare)
+      } 
+      else if(army.mode === 'conquer'){
+        const kingdomOwnerOfTile = state.gameMap[this.coordinates.x][this.coordinates.y].kingdomOwner
+        if(kingdomIdWhichArmyIsLoyalTo === kingdomOwnerOfTile) moveTowardDestination(army, targetSquare)
+      }
+      if(army.turnsOnSameTile >= 2) army.conquerTerritory(state, kingdomIdWhichArmyIsLoyalTo)
+      army.payWages(state, kingdomIdWhichArmyIsLoyalTo)
+      army.turnsOnSameTile++
+    }
+  })
+  console.log(battlesMapping)
+}
+
+
+
+function moveTowardDestination(army, targetSquare){
+  army.coordinates = targetSquare
+  army.turnsOnSameTile = 0
+}
+
+function calculateTargetSquare(army){
+  let targetSquare = army.coordinates
+  if(army.coordinates.x < army.destination.x) targetSquare.x = army.coordinates.x + 1
+  else if (army.coordinates.x > army.destination.x) targetSquare.x = army.coordinates.x - 1
+
+  if(army.coordinates.y < army.destination.y) targetSquare.y = army.coordinates.y + 1
+  else if (army.coordinates.y > army.destination.y) targetSquare.y = army.coordinates.y - 1
+  return targetSquare
+}
+
+//currently the only thing which can block a path is another army
+function checkIfArmyIsBlocking(targetSquare, armyArray, mainArmy){
+  let blockingArmy = null
+  for (const army of armyArray){
+    if(targetSquare.x === army.coordinates.x && targetSquare.y === army.coordinates.y){
+      blockingArmy = army.id
+    }
+  }
+  return blockingArmy
+}
+
+function handleConquerMode(state, kingdomId){
+  const kingdomOwnerOfTile = state.gameMap[this.coordinates.x][this.coordinates.y].kingdomOwner
+  if(kingdomId === kingdomOwnerOfTile) moveTowardDestination(state.armies)
+}
+
+function conquerTerritory(state, kingdomId){
+  state.gameMap[this.coordinates.x][this.coordinates.y].kingdomOwner = kingdomId
 }
 
 export { handleNextTurn }
